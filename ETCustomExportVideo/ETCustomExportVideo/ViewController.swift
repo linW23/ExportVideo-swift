754e62d731886c0c51301613906c5232c91d77b1
linW23 <linwei@eyepetizer.net>
2019-06-24 14:25:12 +0800

    project init

---
+++ ETCustomExportVideo/ETCustomExportVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ETCustomExportVideo
+//
+//  Created by linW23 on 2019/6/24.
+//  Copyright © 2019 LinW23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
